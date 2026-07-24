5dcc6d8109c71b1f2e4e093094c3d6c9a53795f6
Rahul K <rudraofficial900@gmail.com>
2026-07-24 19:05:37 +0530

    Implement basic calculator functionality in Calc2.cpp
    Added a simple calculator program that performs basic arithmetic operations based on user input.

--- 01_Variables/Calculator/Calc2.cpp
+++ 01_Variables/Calculator/Calc2.cpp
@@ -1,1 +1,47 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+   int a, b;
+   char sign;
+   cout<<"----------MENU----------\n";
+   cout<<"1. Addition(+)\n";
+   cout<<"2. Subtraction(-)\n";
+   cout<<"3. Multiplication(*)\n";
+   cout<<"4.Division(/)\n";
+   cout<<"5.Remainder(%)\n";
+   cout<<"-------------------------\n";
+
+   cout<<"Select the operation you wanna perform.\n";
+   cin>>sign;
+   cout<<"Enter the Number 1.\n";
+   cin>>a;
+   cout<<"Enter the Number 2.\n";
+   cin>>b;
+
+   if (sign == '+')
+   {
+    cout<< a + b;
+   }
+   else if (sign == '-')
+   {
+    cout<< a - b;
+   }
+    else if (sign == '*')
+   {
+    cout<< a * b;
+   }
+    else if (sign == '/')
+   {
+    cout<< a / b;
+   }
+    else if (sign == '%')
+   {
+    cout<< a % b;
+   }
+   else
+   cout<<"Invalid selection\n";
+   
+   
+    return 0;
+}
