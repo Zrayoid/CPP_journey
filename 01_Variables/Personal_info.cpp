0183f6150537c7b6cdc6e5eff5197758607da08b
Rahul K <rudraofficial900@gmail.com>
2026-07-24 17:31:46 +0530

    Implement personal information output in Personal_info.cpp
    Display personal details including name, age, weight, and height.

--- 01_Variables/Personal_info.cpp
+++ 01_Variables/Personal_info.cpp
@@ -1,1 +1,13 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    cout<<"Name: Rahul\n ";
+    int age = 20;
+    int weight = 50;
+    float height = 6.1;
+    cout<<"Age:"<< age<<endl;
+    cout<<"Weight:"<<weight<<endl;
+    cout<<"Height:"<<height<<endl;
+
+}
