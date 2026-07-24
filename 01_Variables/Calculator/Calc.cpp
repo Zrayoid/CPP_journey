819ff7ffdf9f9e459ab907b5f15d3ab6d78fdd23
Rahul K <rudraofficial900@gmail.com>
2026-07-24 18:56:36 +0530

    Create Calc.cpp
    This is  basic arithmetic calculation, without user input.

---
+++ 01_Variables/Calculator/Calc.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   int a, b, c;
+   a = 50;
+   b = 20;
+   cout<<"Addition= "<<a+b<<endl;
+   cout<<"Subtraction= "<<a-b<<endl;
+   cout<<"Multiplication= "<<a*b<<endl;
+   cout<<"Division= "<<a/b<<endl;
+   cout<<"Remainder= "<<a%b<<endl;
+    return 0;
+}
