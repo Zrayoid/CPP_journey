fd2a612cf775902cca890313511e3b4ba2fb5438
Rahul K <rudraofficial900@gmail.com>
2026-07-24 18:36:09 +0530

    Implement integer swap using a temporary variable
    Added code to swap two integers using a temporary variable and print their values before and after the swap.

--- 01_Variables/Swap/using_temp_variable.cpp
+++ 01_Variables/Swap/using_temp_variable.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+    int a = 10;
+    int b = 20;
+    int c = 0;
+    cout<<"Value of a before swap  "<< a<<endl;
+    cout<<"Value of b before swap  "<< b<<endl;
+    cout<<"Value of c before swap  "<< c<<endl;  
+     
+    c = a;
+    a = b;
+    b = c;
+    cout<<endl;
+    cout<<endl;
+    cout<<"Value of a after swap  "<< a<<endl;
+    cout<<"Value of b after swap  "<< b<<endl;
+    cout<<"Value of c after swap  "<< c<<endl;
+
+  return 0;
+}
