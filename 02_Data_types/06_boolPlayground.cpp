d73e5d01d5e16eb18b12ddd92c667a11ed87b7bb
Rahul K <rudraofficial900@gmail.com>
2026-07-26 20:50:41 +0530

    Create 06_boolPlayground.cpp for boolean examples
    Add a boolean playground to demonstrate boolean values.

---
+++ 02_Data_types/06_boolPlayground.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    bool a = 1;
+    bool b = 0;
+    bool c = 5;
+    bool d = -10;
+    bool e = true;
+    bool f = false;
+
+    cout<<"a: "<<a<<endl;
+    cout<<"b: "<<b<<endl;
+    cout<<"c: "<<c<<endl;
+    cout<<"d: "<<d<<endl;
+    cout<<"e: "<<e<<endl;
+    cout<<"f: "<<f<<endl;
+
+    return 0;
+}
