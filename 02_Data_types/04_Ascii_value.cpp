a2c9e577c93173f7f6f8ea64ef93f1cb6c3002ae
Rahul K <rudraofficial900@gmail.com>
2026-07-26 19:26:42 +0530

    Add ASCII value display for various characters

---
+++ 02_Data_types/04_Ascii_value.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char a = 'a';
+    char b = '#';
+    char c = '2';
+    char d = '+';
+    char e = '-';
+    char f = ' ';
+
+    cout<<"char a = "<< a << " Ascii is "<< +a <<endl;
+    cout<<"char b = "<< b << " Ascii is "<< +b <<endl;
+
+    cout<<"char c = "<< c << " Ascii is "<< +c <<endl;
+    cout<<"char d = "<< d << " Ascii is "<< +d <<endl;
+   
+    cout<<"char e = "<< e << " Ascii is "<< +e <<endl;
+    cout<<"char f = "<< f << " Ascii is "<< +f <<endl;
+
+    return 0;
+}
