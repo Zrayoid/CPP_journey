73c8749e4498bedea6caebc3a868c5d00faf3bee
Rahul K <rudraofficial900@gmail.com>
2026-07-26 20:42:46 +0530

    Add ASCII table printing for A-Z and a-z
    Prints ASCII values for uppercase and lowercase letters.

---
+++ 02_Data_types/05_ascii_table.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(char i=65; i<=90; i++)
+    {
+        cout<<i<<endl;
+    }
+    cout<<endl;
+    
+    for(char b=97; b<=122; b++)
+    {
+        cout<<b<<endl;  
+    }
+    return 0;
+}
