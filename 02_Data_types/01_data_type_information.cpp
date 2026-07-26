27be2b57c0c9a862d1046b31133006c19c7fa236
Rahul K <rudraofficial900@gmail.com>
2026-07-26 17:38:58 +0530

    Add program to display sizes of various data types

---
+++ 02_Data_types/01_data_type_information.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int a = 4;
+  short b = 3;
+  long c = 2;
+  long long d= 1;
+
+  double e = 44;
+  long double f = 11;
+
+  float h = 2.3;
+
+  char g = 'a';
+
+  bool  i= true;
+  
+  cout<< a <<" The size is: "<<sizeof(a)<<endl;
+  cout<< b <<" The size is: "<<sizeof(b)<<endl;
+  cout<< c <<" The size is: "<<sizeof(c)<<endl;
+  cout<< d <<" The size is: "<<sizeof(d)<<endl;
+  cout<< e <<" The size is: "<<sizeof(e)<<endl;
+  cout<< f <<" The size is: "<<sizeof(f)<<endl;
+  cout<< g <<" The size is: "<<sizeof(g)<<endl;
+  cout<< h <<" The size is: "<<sizeof(h)<<endl;
+  cout<< i <<" The size is: "<<sizeof(i)<<endl;
+ 
+
+
+
+    return 0;
+}
