e8461df8d0e9da29e243be4bd7a6ce5445d24be9
Rahul K <rudraofficial900@gmail.com>
2026-07-27 21:46:53 +0530

    Create equal_or_not.cpp

---
+++ 03_Conditions/equal_or_not.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int firstNum = 10;
+    int secondNum = 10;
+    if(firstNum == secondNum){
+        cout<<"The Numbers are equal.";
+    }
+    else if(firstNum != secondNum){
+        cout<<"The Numbers are not equal";
+    }
+    else
+    cout<<"Ntg";
+
+    return 0;
+}
